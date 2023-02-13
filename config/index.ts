725b94bf012955cf2e90f3115cebceed9862666c
April Dawson <aprilmdawson@hotmail.com>
2023-02-13 23:48:51 +0000

    Update blog link

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@
 export const allowedPrefixes = ["BR", "SW"]
 
 // where is the blog on the web?
-export const blogUrl = "https://example.com"
+export const blogUrl = "http://blog.nowfoster.org"
 
 export const facebookUrl = "https://example.com"
 export const instaUrl = "https://example.com"
