1bbc86f11e154f6c976344fc444285540e3f970f
April <aprilmdawson@hotmail.com>
2023-02-16 23:55:33 +0000

    Add postcode logic

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,18 @@
 // only postcodes starting with these are allowed in
-export const allowedPrefixes = ["BR", "SW"]
+export const allowedPrefixes = [
+    "BR1", "BR2", "BR3", "BR4", "BR5", "BR6", "BR7", 
+    "CM0", "CM1", "CM2", "CM3", "CM4", "CM5", "CM6", "CM7", "CM8", "CM9",
+    "DA14", "DA15", "DA16", "DA17", "DA18", "DA5", "DA6", "DA7", "DA8",
+    "E1", 
+    "EC1", "EC2", "EC3", "EC4",
+    "IG1", "IG2", "IG3", "IG4", "IG5", "IG6", "IG7", "IG8", "IG9",
+    "N1",
+    "NW1", "NW3", "NW4", "NW5", "NW7", "NW8",
+    "RM1", "RM2", "RM3", "RM4", "RM5", "RM6", "RM7", "RM8", "RM9",
+    "SE1", "SE2", "SE3", "SE4", "SE5", "SE6", "SE7", "SE8",
+    "SS0", "SS11", "SS12", "SS13", "SS14", "SS15", "SS16", "SS17", "SS5", "SS6", "SS8", "SS9",
+    "SW1", "SW2", "SW3", "SW4", "SW5", "SW6", "SW7", "SW8", "SW9", 
+    "WC1", "WC2"]
 
 // where is the blog on the web?
 export const blogUrl = "http://blog.nowfoster.org"
