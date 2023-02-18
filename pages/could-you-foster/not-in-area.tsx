4f00fba5d6e8b0875b160f09519940bb2d6b60b1
April <aprilmdawson@hotmail.com>
2023-02-18 19:11:35 +0000

    Add not in area page

---
+++ pages/could-you-foster/not-in-area.tsx
@@ -0,0 +1,41 @@
+import { GetStaticProps } from "next"
+import { Icon } from "../../components/LoaderButton"
+import {
+  CentredQuestion,
+  QuizFooter,
+} from "../../components/QuizLayout"
+import { getQuizContent } from "../../lib/cms"
+import Link from "next/link"
+import s from "./stay-in-touch.module.scss"
+
+const CheckPostcode = () => {
+  return (
+    <>
+    <CentredQuestion>
+      <div>
+        <h1>Sorry, we're hoping to be in your area soon</h1>
+        <p>xxx</p>
+          <div className={s.optionList}>
+            <Link href="https://www.thefosteringnetwork.org.uk/providers" className="squaredButton">Find a local fostering service</Link>
+            <Link href="." className="squaredButton">Join our mailing list to stay in touch</Link>
+          </div>
+      </div>
+    </CentredQuestion>
+    <QuizFooter goBack="/could-you-foster/check-answers">
+    </QuizFooter>
+  </>
+  )
+}
+
+export default CheckPostcode
+
+export const getStaticProps: GetStaticProps = async ({ preview }) => {
+  const quiz = await getQuizContent({ preview: !!preview })
+
+  return {
+    props: {
+      quiz,
+      showPreviewBanner: !!preview,
+    },
+  }
+}
