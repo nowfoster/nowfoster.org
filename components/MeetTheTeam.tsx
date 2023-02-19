2e7f50efed5e9b3f089995bc0a136b276e8f9a2a
Jaye Hackett <jaye.hackett@gmail.com>
2023-02-19 18:59:17 +0000

    refactor meet the team

--- components/MeetTheTeam.tsx
+++ components/MeetTheTeam.tsx
@@ -1,112 +1,59 @@
 import s from "./MeetTheTeam.module.scss"
 
+const Card = () => (
+  <div className={s.card}>
+    <img
+      src="https://images.unsplash.com/photo-1573497161161-c3e73707e25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+      alt=""
+      width={250}
+    />
+    <h2>Jane Doe</h2>
+    <p className="title">CEO &amp; Founder</p>
+    <p>
+      Co-founder of Now Foster. Service Designer and Partner for Impossible
+      Ideas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci
+      lacus, elementum vitae tortor sit amet, bibendum volutpat ex.
+    </p>
+  </div>
+)
+
 const MeetTheTeam = () => (
-    <section className={s.meetTheTeam}>
-      <div className={s.container} id="team">
-        <div className={s.heading}>
-          <h1 className="section-heading">Meet The Team</h1>
-          <p className='section-caption'>Now Foster is a team of innovative social workers, designers and entrepreneurs who want to change how fostering is done.</p>
-        </div>
-        <div className={s.cards}>
-          <div className={s.row}>
-            <div className={s.card}>
-              <img 
-                src="https://images.unsplash.com/photo-1573497161161-c3e73707e25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" 
-                alt="" 
-                width={250}
-              />
-              <h2>Jane Doe</h2>
-              <p className="title">CEO &amp; Founder</p>
-              <p>Co-founder of Now Foster. Service Designer and Partner for Impossible Ideas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci lacus, elementum vitae tortor sit amet, bibendum volutpat ex.</p>
-            </div>
-            <div className={s.card}>
-              <img 
-                src="https://images.unsplash.com/photo-1573497161161-c3e73707e25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" 
-                alt="" 
-                width={250}
-              />
-              <h2>Jane Doe</h2>
-              <p className="title">CEO &amp; Founder</p>
-              <p>Co-founder of Now Foster. Service Designer and Partner for Impossible Ideas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci lacus, elementum vitae tortor sit amet, bibendum volutpat ex.</p>
-            </div>
-            <div className={s.card}>
-              <img 
-                src="https://images.unsplash.com/photo-1573497161161-c3e73707e25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" 
-                alt="" 
-                width={250}
-              />
-              <h2>Jane Doe</h2>
-              <p className="title">CEO &amp; Founder</p>
-              <p>Co-founder of Now Foster. Service Designer and Partner for Impossible Ideas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci lacus, elementum vitae tortor sit amet, bibendum volutpat ex.</p>
-            </div>
-
-          </div>
-          <div className={s.row}>
-            <div className={s.card}>
-              <img 
-                src="https://images.unsplash.com/photo-1573497161161-c3e73707e25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" 
-                alt="" 
-                width={250}
-              />
-              <h2>Jane Doe</h2>
-              <p className="title">CEO &amp; Founder</p>
-              <p>Co-founder of Now Foster. Service Designer and Partner for Impossible Ideas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci lacus, elementum vitae tortor sit amet, bibendum volutpat ex.</p>
-            </div>
-            <div className={s.card}>
-              <img 
-                src="https://images.unsplash.com/photo-1573497161161-c3e73707e25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" 
-                alt="" 
-                width={250}
-              />
-              <h2>Jane Doe</h2>
-              <p className="title">CEO &amp; Founder</p>
-              <p>Co-founder of Now Foster. Service Designer and Partner for Impossible Ideas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci lacus, elementum vitae tortor sit amet, bibendum volutpat ex.</p>
-            </div>
-          </div>
+  <section className={s.meetTheTeam}>
+    <div className={s.container} id="team">
+      <div className={s.heading}>
+        <h1 className="section-heading">Meet The Team</h1>
+        <p className="section-caption">
+          Now Foster is a team of innovative social workers, designers and
+          entrepreneurs who want to change how fostering is done.
+        </p>
+      </div>
+      <div className={s.cards}>
+        <div className={s.row}>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </div>
       </div>
-      <div className={s.container} id="team">
-        <div className={s.heading}>
-          <h1 className="section-heading">Our Board</h1>
-          <p className='section-caption'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id leo dignissim.</p>
-        </div>
-        <div className={s.cards}>
-          <div className={s.row}>
-            <div className={s.card}>
-              <img 
-                src="https://images.unsplash.com/photo-1573497161161-c3e73707e25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" 
-                alt="" 
-                width={250}
-              />
-              <h2>Jane Doe</h2>
-              <p className="title">CEO &amp; Founder</p>
-              <p>Co-founder of Now Foster. Service Designer and Partner for Impossible Ideas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci lacus, elementum vitae tortor sit amet, bibendum volutpat ex.</p>
-            </div>
-            <div className={s.card}>
-              <img 
-                src="https://images.unsplash.com/photo-1573497161161-c3e73707e25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" 
-                alt="" 
-                width={250}
-              />
-              <h2>Jane Doe</h2>
-              <p className="title">CEO &amp; Founder</p>
-              <p>Co-founder of Now Foster. Service Designer and Partner for Impossible Ideas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci lacus, elementum vitae tortor sit amet, bibendum volutpat ex.</p>
-            </div>
-            <div className={s.card}>
-              <img 
-                src="https://images.unsplash.com/photo-1573497161161-c3e73707e25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" 
-                alt="" 
-                width={250}
-              />
-              <h2>Jane Doe</h2>
-              <p className="title">CEO &amp; Founder</p>
-              <p>Co-founder of Now Foster. Service Designer and Partner for Impossible Ideas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc orci lacus, elementum vitae tortor sit amet, bibendum volutpat ex.</p>
-            </div>
-
-          </div>
+    </div>
+    <div className={s.container} id="team">
+      <div className={s.heading}>
+        <h1 className="section-heading">Our Board</h1>
+        <p className="section-caption">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id leo
+          dignissim.
+        </p>
+      </div>
+      <div className={s.cards}>
+        <div className={s.row}>
+          <Card />
+          <Card />
+          <Card />
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 )
 
 export default MeetTheTeam
