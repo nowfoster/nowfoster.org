3fc6e16094287d09e997228048b4c9ce71b9d076
April <aprilmdawson@hotmail.com>
2023-02-20 19:19:57 +0000

    Add linkedin logo to footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -179,7 +179,22 @@ const Layout = ({ children, quiz }: Props) => {
             <nav className={s.socials}>
               Connect
               <div>
-                <Link href={linkedInUrl}>LinkedIn</Link>
+                <Link href={linkedInUrl}>
+                  <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="256"
+                  height="256"
+                  viewBox="0 0 256 256"
+                  >
+                  <g fill="#000" strokeMiterlimit="10" strokeWidth="1">
+                    <path
+                      d="M1.48 29.91h18.657v60.01H1.48V29.91zM10.809.08c5.963 0 10.809 4.846 10.809 10.819 0 5.967-4.846 10.813-10.809 10.813C4.832 21.712 0 16.866 0 10.899 0 4.926 4.832.08 10.809.08M31.835 29.91h17.89v8.206h.255c2.49-4.72 8.576-9.692 17.647-9.692C86.514 28.424 90 40.849 90 57.007V89.92H71.357V60.737c0-6.961-.121-15.912-9.692-15.912-9.706 0-11.187 7.587-11.187 15.412V89.92H31.835V29.91z"
+                      transform="matrix(2.81 0 0 2.81 1.407 1.407)" fill="white"
+                    ></path>
+                    </g>
+                  </svg>
+                  LinkedIn
+                </Link>
                 <Link href={twitterUrl}>
                   <svg width="42" height="36" viewBox="0 0 42 36" fill="none">
                     <path
