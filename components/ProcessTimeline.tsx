d90e7ddfa2673f6ff86efc9633eb2de21c36452c
April <aprilmdawson@hotmail.com>
2023-02-25 13:29:27 +0000

    Change copy for process timeline on How It Works page

--- components/ProcessTimeline.tsx
+++ components/ProcessTimeline.tsx
@@ -11,15 +11,11 @@ const ProcessTimeline = () => (
       <p>A virtual Training session about the basics of fostering. </p>
     </li>
     <li>
-      <h3>Step 3: Application phase (12 weeks)</h3>
-      <p>Application phase that includes home visits and background checks. </p>
+      <h3>Step 3: Train and apply (12 weeks)</h3>
+      <p>Get the paperwork done and train to become a foster carer. </p>
     </li>
     <li>
-      <h3>Step 4: Empathy Training (4 days) </h3>
-      <p>A training course to raise empathy for children in care. </p>
-    </li>
-    <li>
-      <h3>Step 5: Approval panel (1 day)</h3>
+      <h3>Step 4: Approval panel (1 day)</h3>
       <p>A panel to decide if you can become a foster carer. </p>
     </li>
   </ol>
