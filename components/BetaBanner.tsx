c0cc37be52002e150ae64d335818ff1e07cb6fcb
April <aprilmdawson@hotmail.com>
2023-02-27 17:58:53 +0000

    Add feedback form link to Beta Banner

--- components/BetaBanner.tsx
+++ components/BetaBanner.tsx
@@ -3,7 +3,7 @@ import s from "./BetaBanner.module.scss"
 const BetaBanner = () => (
   <section className={s.betaBanner}>
     <div className={s.inner}>
-        <p><strong>Beta.</strong> This is a new service - your <a href=".">feedback</a> will help us improve it.</p>
+        <p><strong>Beta.</strong> This is a new service - your <a href="https://forms.gle/hfMDfMp6UiDXdgfm6">feedback</a> will help us improve it.</p>
     </div>
   </section>
 )
