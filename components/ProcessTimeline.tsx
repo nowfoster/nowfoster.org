6cea7e89fa2b78f404db73dee1baed8367fd0264
Tom Close <tom.close@cantab.net>
2024-04-30 19:44:11 +0100

    change wording for the steps

--- components/ProcessTimeline.tsx
+++ components/ProcessTimeline.tsx
@@ -1,24 +1,24 @@
-import s from "./ProcessTimeline.module.scss"
+import s from "./ProcessTimeline.module.scss";
 
 const ProcessTimeline = () => (
   <ol className={s.timeline}>
     <li>
       <h3>Step 1: Intro Chat (30 min)</h3>
-      <p>A short virtual chat to ask us anything about fostering. </p>
+      <p>A short virtual chat to ask us anything about fostering.</p>
     </li>
     <li>
-      <h3>Step 2: Taster training (3 days)</h3>
-      <p>Virtual training sessions about the basics of fostering. </p>
+      <h3>Step 2: Home visit (2 hours)</h3>
+      <p>A conversation about your circumstances.</p>
     </li>
     <li>
       <h3>Step 3: Train and apply (12 weeks)</h3>
-      <p>Get the paperwork done and train to become a foster carer. </p>
+      <p>Get the paperwork done and train to become a foster carer.</p>
     </li>
     <li>
       <h3>Step 4: Approval panel (1 day)</h3>
-      <p>A panel to decide if you can become a foster carer. </p>
+      <p>A panel to decide if you can become a foster carer.</p>
     </li>
   </ol>
-)
+);
 
-export default ProcessTimeline
+export default ProcessTimeline;
